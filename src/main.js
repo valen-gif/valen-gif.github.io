2e5772ffd685a1bd7bbff2c9f1d108b84745a254
valen-gif <valentin.allen14@gmail.com>
2025-01-01 13:02:55 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
